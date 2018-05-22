fb7bb44b6c65752c00d2914a50e84f468f429ac0
Matt Miller and semhar Kidane <matt@>
2018-05-22 13:21:16 -0700

    Add logic to pass second test.

--- src/main/java/models/RPSGame.java
+++ src/main/java/models/RPSGame.java
@@ -8,9 +8,16 @@ public class RPSGame {
     public String checkWinner(String player1Word, String player2Word) {
         playerWordPair.put(player2Word, "player2");
         playerWordPair.put(player1Word, "palyer1");
-        return playerWordPair.get("paper");
+        if (playerWordPair.containsKey("paper") && playerWordPair.containsKey("rock")){
+            return playerWordPair.get("paper");
+        } else if (playerWordPair.containsKey("scissors") && playerWordPair.containsKey("rock")){
+            return playerWordPair.get("rock");
+        } else if (playerWordPair.containsKey("scissors") && playerWordPair.containsKey("paper")){
+            return playerWordPair.get("scissors");
+        } else {
+            return null;
+        }
     }
-
 
 }
 
