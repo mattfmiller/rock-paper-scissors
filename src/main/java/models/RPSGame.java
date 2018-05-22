b3190e2a22eb9df3722f8031d5d2c6c0ca517b3c
Matt Miller and semhar Kidane <matt@>
2018-05-22 13:32:40 -0700

    Add logic to pass third test.

--- src/main/java/models/RPSGame.java
+++ src/main/java/models/RPSGame.java
@@ -6,16 +6,20 @@ import java.util.Map;
 public class RPSGame {
     Map<String, String> playerWordPair = new HashMap<String, String>();
     public String checkWinner(String player1Word, String player2Word) {
-        playerWordPair.put(player2Word, "player2");
-        playerWordPair.put(player1Word, "palyer1");
-        if (playerWordPair.containsKey("paper") && playerWordPair.containsKey("rock")){
-            return playerWordPair.get("paper");
-        } else if (playerWordPair.containsKey("scissors") && playerWordPair.containsKey("rock")){
-            return playerWordPair.get("rock");
-        } else if (playerWordPair.containsKey("scissors") && playerWordPair.containsKey("paper")){
-            return playerWordPair.get("scissors");
+        if (player1Word.equals(player2Word)){
+            return "draw";
         } else {
-            return null;
+            playerWordPair.put(player2Word, "player2");
+            playerWordPair.put(player1Word, "palyer1");
+            if (playerWordPair.containsKey("paper") && playerWordPair.containsKey("rock")) {
+                return playerWordPair.get("paper");
+            } else if (playerWordPair.containsKey("scissors") && playerWordPair.containsKey("rock")) {
+                return playerWordPair.get("rock");
+            } else if (playerWordPair.containsKey("scissors") && playerWordPair.containsKey("paper")) {
+                return playerWordPair.get("scissors");
+            } else {
+                return null;
+            }
         }
     }
 
